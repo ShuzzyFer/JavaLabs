64823e51b04e7626042354f62816a56fcdb0e5fb
Shuzzy <ShuzzyGames@gmail.com>
2024-04-21 16:39:53 +0300

    Tests

--- src/test/java/com/example/labpokemons/controllers/PokemonControllerTest.java
+++
@@ -1,4 +0,0 @@
-package com.example.labpokemons.controllers;
-
-public class PokemonControllerTest {
-}
--- src/test/java/com/example/labpokemons/services/PokemonServiceDiffblueTest.java
+++ src/test/java/com/example/labpokemons/services/PokemonServiceDiffblueTest.java
@@ -57,43 +57,6 @@ class PokemonServiceDiffblueTest {
     private PokemonService pokemonService;
 
     /**
-     * Method under test: {@link PokemonService#getPokemonsListByParams(String)}
-     */
-    @Test
-    @Disabled("TODO: Complete this test")
-    void testGetPokemonsListByParams() throws IOException {
-        try (MockedStatic<InetAddress> mockInetAddress = mockStatic(InetAddress.class)) {
-            mockInetAddress.when(() -> InetAddress.getAllByName(Mockito.<String>any())).thenReturn(new InetAddress[]{});
-            pokemonService.getPokemonsListByParams("Name");
-        }
-        // TODO: Complete this test.
-        //   Reason: R013 No inputs found that don't throw a trivial exception.
-        //   Diffblue Cover tried to run the arrange/act section, but the method under
-        //   test threw
-        //   java.net.UnknownHostException: okhttp3.Dns$Companion$DnsSystem@609142e2 returned no addresses for pokeapi.co
-        //       at okhttp3.internal.connection.RouteSelector.resetNextInetSocketAddress(RouteSelector.kt:166)
-        //       at okhttp3.internal.connection.RouteSelector.nextProxy(RouteSelector.kt:129)
-        //       at okhttp3.internal.connection.RouteSelector.next(RouteSelector.kt:71)
-        //       at okhttp3.internal.connection.ExchangeFinder.findConnection(ExchangeFinder.kt:205)
-        //       at okhttp3.internal.connection.ExchangeFinder.findHealthyConnection(ExchangeFinder.kt:106)
-        //       at okhttp3.internal.connection.ExchangeFinder.find(ExchangeFinder.kt:74)
-        //       at okhttp3.internal.connection.RealCall.initExchange$okhttp(RealCall.kt:255)
-        //       at okhttp3.internal.connection.ConnectInterceptor.intercept(ConnectInterceptor.kt:32)
-        //       at okhttp3.internal.http.RealInterceptorChain.proceed(RealInterceptorChain.kt:109)
-        //       at okhttp3.internal.cache.CacheInterceptor.intercept(CacheInterceptor.kt:95)
-        //       at okhttp3.internal.http.RealInterceptorChain.proceed(RealInterceptorChain.kt:109)
-        //       at okhttp3.internal.http.BridgeInterceptor.intercept(BridgeInterceptor.kt:83)
-        //       at okhttp3.internal.http.RealInterceptorChain.proceed(RealInterceptorChain.kt:109)
-        //       at okhttp3.internal.http.RetryAndFollowUpInterceptor.intercept(RetryAndFollowUpInterceptor.kt:76)
-        //       at okhttp3.internal.http.RealInterceptorChain.proceed(RealInterceptorChain.kt:109)
-        //       at okhttp3.internal.connection.RealCall.getResponseWithInterceptorChain$okhttp(RealCall.kt:201)
-        //       at okhttp3.internal.connection.RealCall.execute(RealCall.kt:154)
-        //       at com.example.labpokemons.services.OkHttpRequest.get(OkHttpRequest.java:27)
-        //       at com.example.labpokemons.services.PokemonService.getPokemonsListByParams(PokemonService.java:42)
-        //   See https://diff.blue/R013 to resolve this issue.
-    }
-
-    /**
      * Method under test: {@link PokemonService#searchByName(String)}
      */
     @Test
